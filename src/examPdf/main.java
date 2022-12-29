67d8ce37fb9f0c9563ee408e46bd8088ef327d62
alhaddbi <adnan.alhddabi.5@gmail.com>
2022-12-29 04:18:13 -0800

    the last update

--- src/examPdf/main.java
+++ src/examPdf/main.java
@@ -18,18 +18,20 @@ public class main {
 		
 		Scanner sr = new Scanner(System.in);
 		
+		boolean repeat  = true ;
 		
-		System.out.println("Select number from the given numbers");
+		while (repeat) {
+		System.out.println("=========================Select number from the given numbers=====================================");
 		System.out.println("select number 1 to crreat 100 pdf file ");
 		System.out.println("Select number 2 to search for a word ");
+		System.out.println("Select number 0 to stop the program");
+
         int Select = sr.nextInt();
-        
-        
 		if (Select == 1) {
 		try 
 		{
 			int count = 1;
-			for (count = 1 ; count <100 ; count++) {
+			for (count = 0 ; count <=100 ; count++) {
 			Document document = new Document();
 			PdfWriter.getInstance(document, new FileOutputStream("C:\\Users\\Lenovo\\Desktop\\exam\\test"+count+".pdf"));
 			document.open();
@@ -44,19 +46,22 @@ public class main {
 		System.out.println("please cheack this path to find the files C:\\Users\\\\Lenovo\\\\Desktop\\\\exam\\\\test.pdf ");
 		}
 		
+		
+		
 
 		else if(Select == 2) {
+		System.out.println("select the file numebr from ( 1 - 100 ) you want to search");
 		try {
-		System.out.println("select the file numebr you want to search");
 		int enter = sr.nextInt();
 		BufferedReader reader = new BufferedReader(new FileReader("C:\\Users\\Lenovo\\Desktop\\exam\\test"+enter+".pdf"));
 		System.out.println("Enter the word you want to find it ");
 		String find = sr.next();
-		String name1 ;
+		find.split(" ");
+		String s;
 		int count = 0 ;
-		while ((name1 = reader.readLine())!=null)
+		while ((s = reader.readLine())!=null)
 		{
-			if(name1.equals(find))
+			if(s.equals(find))
 			{
 				count++;
 			}
@@ -74,10 +79,23 @@ public class main {
 		}catch (IOException o) {
 			o.printStackTrace();
 		
+		    }
 		
 		}
+		
+		
 
-	}
+	
+		
+		else if (Select == 0)
+		{
+			repeat = false ;
+		}
+		
+		}
+		sr.close();
+		System.out.println("thanks!!!!!!");
+
 		
 	}
 }
